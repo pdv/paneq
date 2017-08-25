09ced23f4d8b54f68d5221dbd128c7309bb49ebe
pdv <philipjdelvecchio@gmail.com>
2017-08-25 10:58:26 -0400

    Setup drawing

--- paneq.js
+++ paneq.js
@@ -5,8 +5,35 @@
 
 'use strict';
 
-const $ = document.getElementById;
+const $ = (id) => document.getElementById(id);
+
+// Graphics
 
 const canvas = $("canvas");
+const gctx = canvas.getContext('2d');
+const w = canvas.width;
+const h = canvas.height;
+
+// Audio
+
+const actx = new window.AudioContext();
 
 
+function drawCenterLine() {
+    gctx.strokeStyle = 'black';
+    gctx.lineWidth = 2;
+    gctx.beginPath();
+    gctx.moveTo(w / 2, 0);
+    gctx.lineTo(w / 2, h);
+    gctx.stroke();
+}
+
+function draw() {
+    drawCenterLine();
+}
+
+function main() {
+    draw();
+}
+
+window.onload = main;
